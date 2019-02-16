dec99be235f75c0504fa05c19689ac1fdaf02fea
eko.zhan <eko.z@outlook.com>
2019-02-16 15:49:57 +0800

    fix: zuul without spring security

--- zxacc-zuul/src/main/java/com/zhengxinacc/zuul/ZxaccZuulApplication.java
+++ zxacc-zuul/src/main/java/com/zhengxinacc/zuul/ZxaccZuulApplication.java
@@ -10,6 +10,15 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
+/**
+ * 使用 exclude 后会有如下报错，直接从 pom.xml 中不引入 security 包，zuul 没必要加上 security 验证
+ * @author eko.zhan
+ * @date 2019/2/16 15:03
+ * Parameter 0 of method setObjectPostProcessor in org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter required a bean of type 'org.springframework.security.config.annotation.ObjectPostProcessor' that could not be found.
+ * @SpringBootApplication(exclude = {
+ * 		org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class
+ * })
+ */
 public class ZxaccZuulApplication {
 
 	public static void main(String[] args) {
